856b290e724d6a59082a72172987a7c8bc06932a
David Sljukic <david.sljukic.dev@gmail.com>
2019-12-18 00:00:01 +0100

    Update storybook webpack config

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -1,38 +1,22 @@
-const path = require("path");
-const glob = require("glob");
-
 module.exports = ({ config, mode }) => {
   config.resolve.symlinks = true;
 
-  config.module.rules.push(
-    {
-      test: /\.js$/,
-      include: [
-        // projectstorm is written for recent versions of the
-        // node engine, so it will not work on older browsers
-        // without transpilation
-        path.resolve(__dirname, "../node_modules/@projectstorm")
-      ],
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: ["@babel/preset-env"]
-      }
-    },
-    {
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("babel-loader"),
-          options: {
-            presets: [["react-app", { flow: false, typescript: true }]]
-          }
-        },
-        {
-          loader: require.resolve("react-docgen-typescript-loader")
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [["react-app", { flow: false, typescript: true }]]
         }
-      ]
-    }
-  );
+      },
+      {
+        loader: require.resolve("react-docgen-typescript-loader")
+      }
+    ]
+  });
+
   config.resolve.extensions.push(".js", ".ts", ".tsx");
+
   return config;
 };
